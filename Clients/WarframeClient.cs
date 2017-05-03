cd76b4916a69b5b6ff5764e5fc67d6c786062e42
Erlite <s45405914@sccb.ac.uk>
2017-05-03 20:03:21 +0100

    Started WarframeClient class

---
+++ Clients/WarframeClient.cs
@@ -0,0 +1,11 @@
+﻿using System;
+
+namespace WarframeNET
+{
+    public class WarframeClient
+    {
+        public event WorldState WorldStateUpdated;
+
+        public WarframeClient() { }
+    }
+}
