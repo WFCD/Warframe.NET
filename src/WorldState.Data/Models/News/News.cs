b1001e5a0f6871f727a7c30e6c78afd617f2035f
Erlite <s45405914@sccb.ac.uk>
2018-11-22 16:06:51 +0000

    Added News model for the worldstate

---
+++ src/WorldState.Data/Models/News/News.cs
@@ -0,0 +1,53 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace WorldState.Data.Models
+{
+    public class News
+    {
+        [JsonProperty]
+        public string Id { get; private set; }
+
+        [JsonProperty]
+        public string Message { get; private set; }
+
+        [JsonProperty]
+        public string Link { get; private set; }
+
+        [JsonProperty]
+        public string ImageLink { get; private set; }
+
+        [JsonProperty("priority")]
+        public bool HasPriority { get; private set; }
+
+        [JsonProperty]
+        public DateTimeOffset Date { get; private set; }
+
+        [JsonProperty("eta")]
+        public string TimeSinceCreationString { get; private set; }
+
+        [JsonProperty("update")]
+        public bool IsUpdate { get; private set; }
+
+        [JsonProperty("primeAccess")]
+        public bool IsPrimeAccess { get; private set; }
+
+        [JsonProperty("stream")]
+        public bool IsStream { get; private set; }
+
+        [JsonProperty]
+        public Dictionary<string, string> Translations { get; private set; }
+
+        [JsonProperty("asString")]
+        public string NewsAsString { get; private set; }
+
+        /// <summary>
+        /// Returns the supposed language of this news instance.
+        /// This is not accurate as it relies on the fact that the first language in the Translations property is the one used.
+        /// </summary>
+        [JsonIgnore]
+        public string PrimaryLanguage { get { return Translations.Keys.First() ?? "Unknown";  } }
+    }
+}
