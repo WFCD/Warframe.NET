809c6444903c5066a5e18cb338db6cfb7723f839
Erlite <s45405914@sccb.ac.uk>
2018-11-24 21:13:23 +0000

    Added GlobalUpgrade

---
+++ src/WorldState.Data/Models/GlobalUpgrade.cs
@@ -0,0 +1,29 @@
+﻿using Newtonsoft.Json;
+using System;
+
+namespace WorldState.Data.Models
+{
+    public class GlobalUpgrade
+    {
+        [JsonProperty("activation")]
+        public DateTimeOffset ActivatedAt { get; private set; }
+
+        [JsonProperty("expiry")]
+        public DateTimeOffset ExpiresAt { get; private set; }
+
+        [JsonProperty]
+        public string Upgrade { get; private set; }
+
+        [JsonProperty]
+        public string Operation { get; private set; }
+
+        [JsonProperty("upgradeOperationValue")]
+        public string OperationValue { get; private set; }
+
+        [JsonProperty("expired")]
+        public bool HasExpired { get; private set; }
+
+        [JsonProperty("eta")]
+        public string TimeRemainingString { get; private set; }
+    }
+}
