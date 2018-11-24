71f01d85e1cff26743a096a5419e23c6291751d3
Erlite <s45405914@sccb.ac.uk>
2018-11-24 19:28:46 +0000

    Added SyndicateMission

---
+++ src/WorldState.Data/Models/Bounty.cs
@@ -0,0 +1,31 @@
+﻿using Newtonsoft.Json;
+
+namespace WorldState.Data.Models
+{
+    public class Bounty
+    {
+        [JsonProperty]
+        public string Id { get; private set; }
+
+        [JsonProperty]
+        public string[] RewardPool { get; private set; }
+
+        [JsonProperty]
+        public string Type { get; private set; }
+
+        [JsonProperty]
+        public int[] EnemyLevels { get; private set; }
+
+        [JsonProperty]
+        public int[] StandingStages { get; private set; }
+
+        [JsonIgnore]
+        public int StageCount { get { return StandingStages.Length; } }
+
+        [JsonIgnore]
+        public int MinimumEnemyLevel { get { return EnemyLevels[0]; } }
+
+        [JsonIgnore]
+        public int MaximumEnemyLevel { get { return EnemyLevels[1]; } }
+    }
+}
---
+++ src/WorldState.Data/Models/Syndicate Missions/SyndicateMission.cs
@@ -0,0 +1,32 @@
+﻿using Newtonsoft.Json;
+using System;
+
+namespace WorldState.Data.Models
+{
+    public class SyndicateMission
+    {
+        [JsonProperty]
+        public string Id { get; private set; }
+
+        [JsonProperty("activation")]
+        public DateTimeOffset ActivatedAt { get; private set; }
+
+        [JsonProperty("expiry")]
+        public DateTimeOffset ExpiresAt { get; private set; }
+
+        [JsonProperty("startString")]
+        public string TimeSinceStartString { get; private set; }
+
+        [JsonProperty]
+        public string Syndicate { get; private set; }
+
+        [JsonProperty]
+        public string[] Nodes { get; private set; }
+
+        [JsonProperty("jobs")]
+        public Bounty[] Bounties { get; private set; }
+
+        [JsonProperty("eta")]
+        public string TimeRemainingString { get; private set; }
+    }
+}
