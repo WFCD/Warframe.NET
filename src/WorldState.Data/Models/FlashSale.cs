8551ad60eb5829daec4177d02229dc7346aebcf5
Younes Ait Amer Meziane <S45405914@sccb.ac.uk>
2018-11-27 13:16:18 +0000

    Added FlashSale

---
+++ src/WorldState.Data/Models/FlashSale.cs
@@ -0,0 +1,32 @@
+using Newtonsoft.Json;
+using System;
+
+namespace WorldState.Data.Models
+{
+	public class FlashSale
+	{
+		[JsonProperty]
+		public string Item { get; private set; }
+
+		[JsonProperty("expiry")]
+		public DateTimeOffset ExpiresAt { get; private set; }
+
+		[JsonProperty]
+		public int Discount { get; private set; }
+
+		[JsonProperty]
+		public int PremiumOverride { get; private set; }
+
+		[JsonProperty]
+		public bool IsFeatured { get; private set; }
+
+		[JsonProperty]
+		public bool IsPopular { get; private set; }
+
+		[JsonProperty]
+		public string Id { get; private set; }
+
+		[JsonProperty("expired")]
+		public bool HasExpired { get; private set; }
+	}
+}
