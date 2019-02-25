6a9712300c97372651f09e995e9b6ece5d016e0e
Yi Ding <3415065+VaslD@users.noreply.github.com>
2019-02-25 18:59:08 +0800

    Add extensions targeting .NET Standard 2.0.

---
+++ WorldState.Extensions/MissionRewardExtensions.cs
@@ -0,0 +1,14 @@
+﻿using System.Drawing;
+
+using WorldState.Data.Models;
+
+namespace WorldState.Extensions
+{
+    public static class MissionRewardExtensions
+    {
+        public static Color GetColorAsFrameworkType(this MissionReward model)
+        {
+            return Color.FromArgb(model.Color);
+        }
+    }
+}
