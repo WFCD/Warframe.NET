54accd167a0b5c227498d1dd34b259619e7e61b6
Yi Ding <3415065+VaslD@users.noreply.github.com>
2019-02-27 19:16:32 +0800

    Add Dark Sector (deprecated) model.

---
+++ src/WorldState.Data/Models/DarkSector.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+using Newtonsoft.Json;
+
+namespace WorldState.Data.Models
+{
+    [Obsolete("This information is not in use anymore. It may be purged from servers.")]
+    public class DarkSector
+    {
+        [JsonProperty]
+        public string Id { get; private set; }
+
+        [JsonProperty]
+        public bool IsAlliance { get; private set; }
+
+        [JsonProperty]
+        public string DefenderName { get; private set; }
+
+        [JsonProperty]
+        public int DefenderDeploymentActivation { get; private set; }
+
+        [JsonProperty]
+        public string DefenderMessage { get; private set; }
+
+        [JsonProperty]
+        public string DeployerName { get; private set; }
+    }
+}
