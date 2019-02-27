b676d7c835447fd5b52cc306a9f06a50976c69e0
Yi Ding <3415065+VaslD@users.noreply.github.com>
2019-02-27 19:29:05 +0800

    Add Void Trader model, currently missing structure for Inventory.

---
+++ src/WorldState.Data/Models/VoidTrader.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+using Newtonsoft.Json;
+
+namespace WorldState.Data.Models
+{
+    public class VoidTrader
+    {
+        [JsonProperty]
+        public string Id { get; private set; }
+
+        [JsonProperty("activation")]
+        public DateTimeOffset ActivatesAt { get; private set; }
+
+        [JsonProperty("expiry")]
+        public DateTimeOffset ExpiresAt { get; private set; }
+
+        // Should compare with DateTime.Now?
+        [JsonProperty("active")]
+        public bool IsActive { get; private set; }
+
+        [JsonProperty("character")]
+        public string CharacterName { get; private set; }
+
+        [JsonProperty]
+        public string Location { get; private set; }
+
+        // What is this?
+        [JsonProperty]
+        protected string PsId { get; private set; }
+
+        // Todo: Inventory. No docs.
+    }
+}
