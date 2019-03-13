27f33fa6b5391c97252cef15da510a398b097a4a
Jooty <compundbread@gmail.com>
2019-03-13 07:56:09 -0400

    Create CetusCycle.cs

---
+++ src/WorldState.Data/Models/CetusCycle.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Newtonsoft.Json;
+
+namespace WorldState.Data.Models
+{
+    public class CetusCycle
+    {
+        [JsonProperty]
+        public string Id { get; private set; }
+
+        [JsonProperty]
+        public DateTimeOffset Expiry { get; private set; }
+
+        [JsonProperty]
+        public bool IsDay { get; private set; }
+
+        [JsonProperty]
+        public string TimeLeft { get; private set; }
+
+        [JsonProperty]
+        public bool IsCetus { get; private set; }
+    }
+}
