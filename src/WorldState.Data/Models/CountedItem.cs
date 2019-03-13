0686ac0b7a7eb8336dd8779d9a5e42aeea3fd14d
Jooty <compundbread@gmail.com>
2019-03-13 08:06:36 -0400

    Added multiple models.
    - Added CountedItem.
    - Added Reward.
    - Added Invasion.

---
+++ src/WorldState.Data/Models/CountedItem.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Newtonsoft.Json;
+
+namespace WorldState.Data.Models
+{
+    public class CountedItem
+    {
+        [JsonProperty]
+        public int Count { get; private set; }
+
+        [JsonProperty]
+        public string Type { get; private set; }
+    }
+}
---
+++ src/WorldState.Data/Models/Reward.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Newtonsoft.Json;
+
+namespace WorldState.Data.Models
+{
+    public class Reward
+    {
+        [JsonProperty]
+        public string[] Items { get; private set; }
+
+        [JsonProperty]
+        public CountedItem[] CountedItems { get; private set; }
+
+        [JsonProperty]
+        public int Credits { get; private set; }
+    }
+}
