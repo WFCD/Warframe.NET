359d77af1644ead0085527631f5a5096636f0460
Yi Ding <3415065+VaslD@users.noreply.github.com>
2019-11-06 17:13:36 +0800

    New model: Orb Vallis

---
+++ src/WorldState.Data/Enums/OrbVallisSeason.cs
@@ -0,0 +1,15 @@
+﻿using System.Runtime.Serialization;
+
+namespace WorldState.Data.Enums
+{
+    public enum OrbVallisSeason
+    {
+        Unknown,
+
+        [EnumMember(Value = "cold")]
+        Cold,
+
+        [EnumMember(Value = "warm")]
+        Warm
+    }
+}
