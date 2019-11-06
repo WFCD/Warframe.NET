960a8d249ae3ee71f6599fed5d40fbc89b6749bf
Yi Ding <3415065+VaslD@users.noreply.github.com>
2019-11-06 17:05:38 +0800

    New interface and extension: ITimeSensitive
    For models that have both a start/activation and an end/expiration timestamp, implementing this interface automatically provides IsActive(), HasExpired(), and GetTimeRemaining() methods.

---
+++ src/WorldState.Data/Interfaces/ITimeSensitive.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+namespace WorldState.Data.Interfaces
+{
+    /// <summary>
+    /// Models implementing this interface has time-sensitive data that may change as real-world (wall-clock) time passes.
+    /// Consumers should check the <see cref="ExpiresAt"/> property frequently and obtain updated models when available.
+    /// </summary>
+    public interface ITimeSensitive
+    {
+        DateTimeOffset ActivatedAt { get; }
+        DateTimeOffset ExpiresAt   { get; }
+    }
+
+    public static class TimeSensitiveModelExtensions
+    {
+        public static bool IsActive(this ITimeSensitive model)
+        {
+            var now = DateTimeOffset.Now;
+            return now >= model.ActivatedAt && now < model.ExpiresAt;
+        }
+
+        public static bool HasExpired(this ITimeSensitive model)
+        {
+            return DateTimeOffset.Now >= model.ExpiresAt;
+        }
+
+        public static TimeSpan GetTimeRemaining(this ITimeSensitive model)
+        {
+            return model.ExpiresAt - DateTimeOffset.Now;
+        }
+    }
+}
