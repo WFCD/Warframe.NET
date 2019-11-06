dbf8772fb0cf1335d9c9b053b49c4d02995e2af4
Yi Ding <3415065+VaslD@users.noreply.github.com>
2019-11-06 17:14:21 +0800

    New model: Invasion

---
+++ src/WorldState.Data/Models/Invasion.cs
@@ -0,0 +1,42 @@
+﻿using System;
+
+using Newtonsoft.Json;
+
+namespace WorldState.Data.Models
+{
+    public class Invasion
+    {
+        [JsonProperty]
+        public string Id { get; private set; }
+
+        [JsonProperty("desc")]
+        public string Name { get; private set; }
+
+        [JsonProperty]
+        public string Node { get; private set; }
+
+        [JsonProperty("activation")]
+        public DateTimeOffset ActivatedAt { get; private set; }
+
+        [JsonProperty]
+        public string[] AttackingFaction { get; private set; }
+
+        [JsonProperty]
+        public string[] DefendingFaction { get; private set; }
+
+        [JsonProperty("count")]
+        public int CurrentValue { get; private set; }
+
+        [JsonProperty("requiredRuns")]
+        public int TargetValue { get; private set; }
+
+        [JsonProperty]
+        public MissionReward[] AttackerRewards { get; private set; }
+
+        [JsonProperty]
+        public MissionReward[] DefenderRewards { get; private set; }
+
+        [JsonProperty("vsInfestation")]
+        public bool IsVersusInfestation { get; private set; }
+    }
+}
