7bf3acf752233cad8db455dd16e1ae51be5ed64e
Yi Ding <3415065+VaslD@users.noreply.github.com>
2019-11-20 20:36:24 +0800

    WorldStateClient now functional.

--- src/WorldState/WorldStateClient.cs
+++ src/WorldState/WorldStateClient.cs
@@ -1,9 +1,11 @@
 ﻿using System;
+using System.Collections.Generic;
 using System.IO;
 using System.Net.Http;
 using System.Threading.Tasks;
 
 using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
 
 using WorldState.Data.Models;
 
@@ -11,8 +13,9 @@ namespace WorldState
 {
     public class WorldStateClient : IDisposable
     {
-        public HttpClient HttpClient { get; }
-        public Platform   Platform   { get; }
+        public Platform Platform { get; }
+
+        internal HttpClient HttpClient { get; }
 
         private JsonSerializer serializer;
         private UriBuilder     builder;
@@ -95,10 +98,118 @@ namespace WorldState
             return Get<ConclaveChallenge[]>(builder);
         }
 
+        public Task<DailyDeal[]> DailyDeals()
+        {
+            builder.Path = platformAsString + WorldStateEndpoints.DailyDeal;
+            return Get<DailyDeal[]>(builder);
+        }
+
+        public Task<EarthCycle> EarthCycle()
+        {
+            builder.Path = platformAsString + WorldStateEndpoints.EarthRotation;
+            return Get<EarthCycle>(builder);
+        }
+
+        public Task<Event[]> Events()
+        {
+            builder.Path = platformAsString + WorldStateEndpoints.Events;
+            return Get<Event[]>(builder);
+        }
+
+        public Task<FlashSale[]> FlashSales()
+        {
+            builder.Path = platformAsString + WorldStateEndpoints.FlashSales;
+            return Get<FlashSale[]>(builder);
+        }
+
         public Task<ConstructionProgress> FleetConstructionProgress()
         {
             builder.Path = platformAsString + WorldStateEndpoints.Construction;
             return Get<ConstructionProgress>(builder);
+        }
+
+        public Task<GlobalUpgrade[]> GlobalUpgrades()
+        {
+            builder.Path = platformAsString + WorldStateEndpoints.GlobalBoosts;
+            return Get<GlobalUpgrade[]>(builder);
+        }
+
+        public Task<Invasion[]> Invasions()
+        {
+            builder.Path = platformAsString + WorldStateEndpoints.Invasions;
+            return Get<Invasion[]>(builder);
+        }
+
+        public Task<News[]> News()
+        {
+            builder.Path = platformAsString + WorldStateEndpoints.News;
+            return Get<News[]>(builder);
+        }
+
+        public Task<Nightwave> Nightwave()
+        {
+            builder.Path = platformAsString + WorldStateEndpoints.Nightwave;
+            return Get<Nightwave>(builder);
+        }
+
+        public Task<OrbVallisCycle> OrbVallisCycle()
+        {
+            builder.Path = platformAsString + WorldStateEndpoints.OrbVallis;
+            return Get<OrbVallisCycle>(builder);
+        }
+
+        public async Task<RivenMod[]> RivenMods()
+        {
+            builder.Path = platformAsString + WorldStateEndpoints.Rivens;
+            var json = (await Get<JToken>(builder).ConfigureAwait(false));
+
+            if (json == null) return Array.Empty<RivenMod>();
+
+            var mods = new List<RivenMod>(350);
+            foreach (var category in json.Values())
+            {
+                foreach (var weapon in category.Values())
+                {
+                    foreach (var variant in weapon.Values())
+                    {
+                        if (!variant.HasValues) continue;
+
+                        mods.Add(variant.ToObject<RivenMod>());
+                    }
+                }
+            }
+
+            return mods.ToArray();
+        }
+
+        public Task<Simaris> SimarisSanctuary()
+        {
+            builder.Path = platformAsString + WorldStateEndpoints.Sanctuary;
+            return Get<Simaris>(builder);
+        }
+
+        public Task<Sortie[]> Sorties()
+        {
+            builder.Path = platformAsString + WorldStateEndpoints.Sortie;
+            return Get<Sortie[]>(builder);
+        }
+
+        public Task<SyndicateMission[]> SyndicateMissions()
+        {
+            builder.Path = platformAsString + WorldStateEndpoints.SyndicateMissions;
+            return Get<SyndicateMission[]>(builder);
+        }
+
+        public Task<Fissure[]> VoidFissureMissions()
+        {
+            builder.Path = platformAsString + WorldStateEndpoints.VoidMissions;
+            return Get<Fissure[]>(builder);
+        }
+
+        public Task<VoidTrader> VoidTrader()
+        {
+            builder.Path = platformAsString + WorldStateEndpoints.VoidTrader;
+            return Get<VoidTrader>(builder);
         }
 
         #endregion
--- src/WorldState/WorldStateEndpoints.cs
+++ src/WorldState/WorldStateEndpoints.cs
@@ -2,11 +2,28 @@
 {
     internal static class WorldStateEndpoints
     {
-        public const string Base         = "https://api.warframestat.us";
-        public const string Alerts       = "/alerts";
-        public const string Acolytes     = "/persistentEnemies";
-        public const string CetusCycle   = "/cetusCycle";
-        public const string Conclaves    = "/conclaveChallenges";
-        public const string Construction = "/constructionProgress";
+        public const string Base              = "https://api.warframestat.us";
+        public const string Alerts            = "/alerts";
+        public const string Acolytes          = "/persistentEnemies";
+        public const string CetusCycle        = "/cetusCycle";
+        public const string Conclaves         = "/conclaveChallenges";
+        public const string Construction      = "/constructionProgress";
+        public const string DailyDeal         = "/dailyDeals";
+        public const string EarthRotation     = "/earthCycle";
+        public const string Events            = "/events";
+        public const string FlashSales        = "/flashSales";
+        public const string GlobalBoosts      = "/globalUpgrades";
+        public const string Invasions         = "/invasions";
+        public const string News              = "/news";
+        public const string Nightwave         = "/nightwave";
+        public const string OrbVallis         = "/vallisCycle";
+        public const string Rivens            = "/rivens";
+        public const string Sanctuary         = "/simaris";
+        public const string Sortie            = "/sortie";
+        public const string SyndicateMissions = "/syndicateMissions";
+        public const string VoidMissions      = "/fissures";
+        public const string VoidTrader        = "/voidTrader";
+
+        public const string RivenQuery = "/rivens/search/";
     }
 }
