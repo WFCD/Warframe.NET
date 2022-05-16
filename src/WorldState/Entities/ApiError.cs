f4dbc946cab3d58da96404cbac77b29d11d1e6b9
Younes Meziane <ys.aameziane@gmail.com>
2022-05-16 13:45:25 +0200

    Add ApiError entity, split WS entities into their own subfolder.

---
+++ src/WorldState/Entities/ApiError.cs
@@ -0,0 +1,23 @@
+﻿using System.Net;
+using Newtonsoft.Json;
+
+namespace WorldState.Entities
+{
+    /// <summary>
+    /// Returned by the warframestat.us API when an error has occurred.
+    /// </summary>
+    public class ApiError
+    {
+        /// <summary>
+        /// A short description of the error.
+        /// </summary>
+        [JsonProperty("error")]
+        public string Error { get; private set; }
+
+        /// <summary>
+        /// The HTTP status code that was returned alongside the error.
+        /// </summary>
+        [JsonProperty("code")]
+        public HttpStatusCode Code { get; private set; }
+    }
+}
