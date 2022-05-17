4c046bab853776366d2bd73bc3b66d050d42e760
Younes Meziane <ys.aameziane@gmail.com>
2022-05-18 01:15:25 +0200

    Add ApiResult class (root endpoint)

--- src/WorldState/Entities/ApiResult.cs
+++ src/WorldState/Entities/ApiResult.cs
@@ -1,7 +1,23 @@
-﻿namespace WorldState.Entities
+﻿using System.Net;
+using Newtonsoft.Json;
+
+namespace WorldState.Entities
 {
+    /// <summary>
+    /// Generally returned by the warframestat.us API when no error or results need to be returned.
+    /// </summary>
     public class ApiResult
     {
-        
+        /// <summary>
+        /// Message sent by the API.
+        /// </summary>
+        [JsonProperty("message")]
+        public string Message { get; private set; }
+
+        /// <summary>
+        /// The HTTP status code that was returned alongside the message.
+        /// </summary>
+        [JsonProperty("code")]
+        public HttpStatusCode Code { get; private set; }
     }
 }
