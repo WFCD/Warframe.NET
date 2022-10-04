f44875334200d0a5bc16c6221fd3ab6631bd6f42
Erlite <ys.aameziane@gmail.com>
2022-10-04 21:50:55 +0200

    Add trailing slash at the end of the endpoint path.

--- src/WorldState/Enums/WorldStateEndpoint.cs
+++ src/WorldState/Enums/WorldStateEndpoint.cs
@@ -17,166 +17,166 @@ namespace WarframeNet.WorldState.Enums
         /// Retrieves the last update time of the world state.
         /// Returns a <see cref="System.DateTimeOffset"/> object.
         /// </summary>
-        [EndpointPath("/timestamp")]
+        [EndpointPath("/timestamp/")]
         Timestamp,
         /// <summary>
         /// Currently available in-game Warframe news.
         /// Returns a list of <see cref="WarframeNet.WorldState.News"/> objects.
         /// </summary>
-        [EndpointPath("/news")]
+        [EndpointPath("/news/")]
         News,
         /// <summary>
         /// Ongoing in-game events.
         /// Returns a list of <see cref="WarframeNet.WorldState.WorldEvent"/> objects.
         /// </summary>
-        [EndpointPath("/events")]
+        [EndpointPath("/events/")]
         Events,
         /// <summary>
         /// Ongoing and upcoming in-game alerts.
         /// Returns a list of <see cref="WarframeNet.WorldState.Alert"/> objects.
         /// </summary>
-        [EndpointPath("/alerts")]
+        [EndpointPath("/alerts/")]
         Alerts,
         /// <summary>
         /// Information about the current world sortie.
         /// </summary>
         /// Returns a <see cref="WarframeNet.WorldState.Sortie"/> object.
-        [EndpointPath("/sortie")]
+        [EndpointPath("/sortie/")]
         Sortie,
         /// <summary>
         /// Currently available syndicate missions.
         /// </summary>
         /// Returns a list of <see cref="WarframeNet.WorldState.SyndicateMission"/> objects.
-        [EndpointPath("/syndicateMissions")]
+        [EndpointPath("/syndicateMissions/")]
         SyndicateMissions,
         /// <summary>
         /// Currently available fissures
         /// </summary>
         /// Returns a list of <see cref="WarframeNet.WorldState.Fissure"/> objects.
-        [EndpointPath("/fissures")]
+        [EndpointPath("/fissures/")]
         Fissures,
         /// <summary>
         /// Represents an upgrade that applies to all players.
         /// </summary>
         /// Returns a list of <see cref="WarframeNet.WorldState.GlobalUpgrade"/> objects.
-        [EndpointPath("/globalUpgrades")]
+        [EndpointPath("/globalUpgrades/")]
         GlobalUpgrades,
         /// <summary>
         /// Represents a flash sale.
         /// </summary>
         /// Returns a list of <see cref="WarframeNet.WorldState.FlashSale"/> objects.
-        [EndpointPath("/flashSales")]
+        [EndpointPath("/flashSales/")]
         FlashSales,
         /// <summary>
         /// Represents an invasion with attacking and defending factions.
         /// </summary>
         /// Returns a list of <see cref="WarframeNet.WorldState.Invasion"/> objects.
-        [EndpointPath("/invasions")]
+        [EndpointPath("/invasions/")]
         Invasion,
         /// <summary>
         /// Used to represent dark sectors clans could fight to gain control over.
         /// </summary>
         [Obsolete("Dark sectors are under permanent armistice, as such the result will always be empty. Please don't hit against the API for no reason.", true)]
-        [EndpointPath("/darkSectors")]
+        [EndpointPath("/darkSectors/")]
         DarkSectors,
         /// <summary>
         /// Current information about the void trader's time and place of arrival, as well as inventory if available.
         /// </summary>
         /// Returns a <see cref="WarframeNet.WorldState.VoidTrader"/> object.
-        [EndpointPath("/voidTrader")]
+        [EndpointPath("/voidTrader/")]
         VoidTrader,
         /// <summary>
         /// Represents a daily deal, which generally brings discounts to specific items.
         /// </summary>
         /// Returns a list of <see cref="WarframeNet.WorldState.DailyDeal"/> objects.
-        [EndpointPath("/dailyDeals")]
+        [EndpointPath("/dailyDeals/")]
         DailyDeals,
         /// <summary>
         /// Contains information about sanctuary targets.
         /// </summary>
         /// Returns a <see cref="WarframeNet.WorldState.Simaris"/> object.
-        [EndpointPath("/simaris")]
+        [EndpointPath("/simaris/")]
         Simaris,
         /// <summary>
         /// Represents a Conclave challenge.
         /// </summary>
         /// Returns a list of <see cref="WarframeNet.WorldState.ConclaveChallenge"/> objects.
-        [EndpointPath("/conclaveChallenges")]
+        [EndpointPath("/conclaveChallenges/")]
         ConclaveChallenges,
         /// <summary>
         /// @todo: document this
         /// </summary>
         /// Returns a list of <see cref="WarframeNet.WorldState.PersistentEnemy"/> objects.
-        [EndpointPath("/persistentEnemies")]
+        [EndpointPath("/persistentEnemies/")]
         PersistentEnemies,
         /// <summary>
         /// Current information about the day/night cycle of Earth.
         /// </summary>
         /// Returns an <see cref="WarframeNet.WorldState.EarthCycle"/> object.
-        [EndpointPath("/earthCycle")]
+        [EndpointPath("/earthCycle/")]
         EarthCycle,
         /// <summary>
         /// Current information about the day/night cycle of Cetus.
         /// </summary>
         /// Returns a <see cref="WarframeNet.WorldState.CetusCycle"/> object.
-        [EndpointPath("/cetusCycle")]
+        [EndpointPath("/cetusCycle/")]
         CetusCycle,
         /// <summary>
         /// Current information about the Fass/Vome cycle of the Cambion Drift.
         /// </summary>
         /// Returns a <see cref="WarframeNet.WorldState.CambionCycle"/> object.
-        [EndpointPath("/cambionCycle")]
+        [EndpointPath("/cambionCycle/")]
         CambionCycle,
         /// <summary>
         /// Current information about the warm/cold cycle of the Orb Vallis.
         /// </summary>
         /// Returns a <see cref="WarframeNet.WorldState.VallisCycle"/> object.
-        [EndpointPath("/vallisCycle")]
+        [EndpointPath("/vallisCycle/")]
         VallisCycle,
         [Obsolete("This endpoint seemed to have been used for the Void Trader at one point, and is no longer in use.", true)]
-        [EndpointPath("/weeklyChallenges")]
+        [EndpointPath("/weeklyChallenges/")]
         WeeklyChallenges,
         /// <summary>
         /// Current information about the construction of Fomorians and Razorbacks. 
         /// </summary>
         /// Returns a <see cref="WarframeNet.WorldState.ConstructionProgress"/> object.
-        [EndpointPath("/constructionProgress")]
+        [EndpointPath("/constructionProgress/")]
         ConstructionProgress,
         /// <summary>
         /// Current information about ongoing Nightwave challenges and rewards.
         /// </summary>
         /// Returns a <see cref="WarframeNet.WorldState.Nightwave"/> object.
-        [EndpointPath("/nightwave")]
+        [EndpointPath("/nightwave/")]
         Nightwave,
         /// <summary>
         /// Currently active Kuva missions.
         /// </summary>
         /// Returns a list of <see cref="WarframeNet.WorldState.Kuva"/> objects.
-        [EndpointPath("/kuva")]
+        [EndpointPath("/kuva/")]
         Kuva,
         /// <summary>
         /// Retrieves information about the current Arbitration.
         /// </summary>
         /// Returns an <see cref="WarframeNet.WorldState.Arbitration"/> object.
-        [EndpointPath("/arbitration")]
+        [EndpointPath("/arbitration/")]
         Arbitration,
         /// <summary>
         /// Represents a set of sentient outposts that are present
         /// </summary>
         /// Returns a <see cref="WarframeNet.WorldState.SentientOutpost"/> object.
-        [EndpointPath("/sentientOutposts")]
+        [EndpointPath("/sentientOutposts/")]
         SentientOutposts,
         /// <summary>
         /// Current information about Steel Path rotation, incursion and rewards.
         /// </summary>
         /// Returns a <see cref="WarframeNet.WorldState.SteelPath"/> object.
-        [EndpointPath("/steelPath")]
+        [EndpointPath("/steelPath/")]
         SteelPath,
         /// <summary>
         /// Information about currently sold vaulted items at Maroo's Bazaar.
         /// </summary>
         /// Returns a <see cref="WarframeNet.WorldState.VaultTrader"/> object.
-        [EndpointPath("/vaultTrader")]
+        [EndpointPath("/vaultTrader/")]
         VaultTrader,
     }
 }
